52a9829205c7128a6d3368e8a76c47587e861934
Pedro Henrique Ernandes da Silva <thepedrinhoo@gmail.com>
2025-02-07 14:23:03 -0300

    Criando todas as rotas de CRUD da aplicação

--- src/app.js
+++ src/app.js
@@ -14,6 +14,12 @@ const livros = [
     }
 ]
 
+function buscaLivroPorId(id) {
+    return livros.findIndex(livro => {
+        return livro.id === Number(id)
+    })
+}
+
 app.get("/", (req, res) => {
     res.status(200).send("Curso de Node.JS");
 });
@@ -23,9 +29,32 @@ app.get("/livros", (req, res) => {
     res.status(200).json(livros);
 });
 
+// Rota para listar livros de acordo com o ID passado no EndPoint
+app.get("/livros/:id", (req, res) => {
+    const indexLivro = buscaLivroPorId(req.params.id); // Params propriedade que vem do obj req
+    res.status(200).json(livros[indexLivro]); // Exibindo o livro com o Index encontrado
+});
+
+// Rota para criar um novo livro no array de objetos
 app.post("/livros", (req, res) => {
     livros.push(req.body);
     res.status(201).send("Livro Cadastrado com sucesso!");
+});
+
+// Rota para alterar put ou patch um livro existente
+app.put("/livros/:id", (req, res) => {
+    const indexLivro = buscaLivroPorId(req.params.id);
+    livros[indexLivro].titulo = req.body.titulo // substituindo o titulo de um objeto para o passado na API
+
+    res.status(200).json(livros);
+});
+
+// Rota para deletar um livro existente
+app.delete("/livros/:id", (req, res) => {
+    const indexLivro = buscaLivroPorId(req.params.id);
+    livros.splice(indexLivro, 1) // Splice localiza um elemento e deleta este elemento
+    
+    res.status(200).send("Livro deletado com sucesso!");
 });
 
 export default app; // Exportando o modulo para usar em Server.JS
